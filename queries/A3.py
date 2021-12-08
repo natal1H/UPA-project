7ce374fd16f1e8ca25f9981dab7e2791f0b42ffb
Natália Holková <xholko02@stud.fit.vutbr.cz>
2021-12-08 21:17:02 +0100

    A3 query csv extracted

--- queries/A3.py
+++ queries/A3.py
@@ -7,9 +7,7 @@ def A3_extract_csv(db, csv_location="A3.csv"):
     pipeline = [
         {"$match":
              {'pohlavi':
-                  {"$in": ['Z','M']},
-              # 'vekova_skupina':
-              #     {"$in": ['70-74', '60-64']},
+                  {"$in": ['Z', 'M']},
               }
          },
 
@@ -20,24 +18,46 @@ def A3_extract_csv(db, csv_location="A3.csv"):
         }},
         {"$group":
             {
-                "_id": { 'kraj_nuts_kod': '$kraj_nuts_kod', 'pohlavi': '$pohlavi', 'vekova_skupina': '$vekova_skupina'},
+                "_id": {'kraj_nuts_kod': '$kraj_nuts_kod', 'pohlavi': '$pohlavi', 'vekova_skupina': '$vekova_skupina'},
                 "count": {"$sum": 1},
             }
         },
         {"$sort": {"kraj_nuts_kod": 1}}
     ]
 
+    res_vaccinated = db.vaccinated_by_profession.aggregate(pipeline)
+    res_vaccinated = dumps(list(res_vaccinated))
+    res_vaccinated = json.loads(res_vaccinated)
 
-    df = db.vaccinated_by_profession.aggregate(pipeline)
-    df = dumps(list(df))
-    df = json.loads(df)
+    df_vaccinated = pd.json_normalize(res_vaccinated)
+    df_vaccinated = df_vaccinated.rename(columns={"_id.kraj_nuts_kod": "cznuts"})
 
-    # pd_data_2020 = pd.DataFrame.from_dict(data_2020)
-    df = pd.json_normalize(df)
-    df = df.rename(
-        columns={"_id.kraj_nuts_kod": "cznuts"})
+    df_vaccinated = df_vaccinated[df_vaccinated['cznuts'].notna()]
 
-    df = df[df['cznuts'].notna()]
+    # Get regions from mongodb
+    pipeline = [
+        {"$project": {
+            "cznuts": 1,
+            "text": 1
+        }}
+    ]
+
+    res_regions = db.region_enumerator.aggregate(pipeline)
+    res_regions = dumps(list(res_regions))
+    res_regions = json.loads(res_regions)
+    df_regions = pd.json_normalize(res_regions)
+
+    # Rename region codes in vaccinated dataframe to actual text names
+    for region_code in df_regions['cznuts'].unique():
+        text_name = df_regions[df_regions["cznuts"] == region_code]["text"].values[0]
+        df_vaccinated['cznuts'] = [text_name if code == region_code else code for code in df_vaccinated['cznuts']]
+
+    # Rename columns in vaccinated dataframe
+    df_vaccinated = df_vaccinated.rename(columns={"cznuts": "region", "_id.pohlavi": "gender",
+                                                  "_id.vekova_skupina": "age_group"})
+    df_vaccinated = df_vaccinated.set_index('region') # Ok?
+    df_vaccinated.to_csv(csv_location, sep=';', encoding='utf-8')
+
 
 def A3_plot_graph(csv_location="A3.csv", save_location="A3.png"):
     pass
